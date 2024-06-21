c229cab25cb6e66b0f568a1de545cf4ce78d4d95
Thanarit <u6410322@au.edu>
2024-06-21 15:09:34 +0700

    refactor: Update VAT Calculator user input handling

--- js/VATCalculator.js
+++ js/VATCalculator.js
@@ -1,11 +1,14 @@
 // Build VAT Calculator
 
 function calculateVAT() {
-    var grossPrice = parseFloat(document.getElementById("grossPrice").value);
+    gp = document.getElementById("grossPrice").value;
+    var grossPrice = parseFloat(gp);
     var vatRate = 0.07; // 7% VAT rate
     var vatAmount = grossPrice * vatRate;
     document.getElementById("vatAmount").textContent = " $" + vatAmount.toFixed(2);
     document.getElementById("TotalAmount").textContent = " $" + (grossPrice + vatAmount).toFixed(2);
+
+    console.table({gp, grossPrice, vatRate, vatAmount});
         }
 
 
