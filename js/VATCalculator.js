3a7b4fc29ad3208fc7dcc20d41df100d3e9b1083
Thanarit <u6410322@au.edu>
2024-06-21 14:50:43 +0700

    refine a bit more

--- js/VATCalculator.js
+++ js/VATCalculator.js
@@ -4,7 +4,7 @@ function calculateVAT() {
     var grossPrice = parseFloat(document.getElementById("grossPrice").value);
     var vatRate = 0.07; // 7% VAT rate
     var vatAmount = grossPrice * vatRate;
-    document.getElementById("vatAmount").textContent = "$ " + vatAmount.toFixed(2);
-    document.getElementById("TotalAmount").textContent = "$ " + (grossPrice + vatAmount).toFixed(2);
+    document.getElementById("vatAmount").textContent = " $ " + vatAmount.toFixed(2);
+    document.getElementById("TotalAmount").textContent = " $ " + (grossPrice + vatAmount).toFixed(2);
         }
 
